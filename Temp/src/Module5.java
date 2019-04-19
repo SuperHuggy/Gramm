b242f38591caf317447631c903a2252cf401b8c1
SuperHuggy <stolyarov_danil@mail.ru>
2019-04-19 11:27:00 +0300

    Модуль 5

--- Temp/src/Module5.java
+++ Temp/src/Module5.java
@@ -19,9 +19,22 @@ public class Module5
         for (int i = 0; i < n.length; i++)
             buf[i] = exec.submit(new MultyKMP(h, n[i]));
         Daemon daemon = new Daemon(buf);
-        timer.schedule(daemon, 0, 1000);
-        while (daemon.getNotDone() != 0)
-            System.out.println(daemon.getNotDone());
+        timer.schedule(daemon, 0, 100);
+        anotherDaemon aD = new anotherDaemon(buf);
+        Thread d = new Thread(aD);
+        d.setDaemon(true);
+        d.start();
+        while (daemon.getNotDone() != 0 || aD.getNotDone() != 0)
+        {
+            System.out.println(daemon.getNotDone() + "   " + aD.getNotDone());
+            try
+            {
+                Thread.sleep(100);
+            } catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
         timer.cancel();
         int i = 0;
         exec.shutdown();
@@ -48,7 +61,7 @@ public class Module5
 class Daemon extends TimerTask
 {
     private Future[] exec;
-    private int notDone;
+    private int notDone = -1;
 
     Daemon(Future[] exe)
     {
@@ -56,17 +69,50 @@ class Daemon extends TimerTask
     }
 
     @Override
-    public void run()
+    synchronized public void run()
     {
-        notDone = 0;
+        int b = 0;
         for (Future buf : exec)
             if (!buf.isDone())
-                notDone++;
-        System.out.println(notDone);
+                b++;
+        notDone = b;
     }
 
     public int getNotDone()
     {
+        while (notDone == -1)
+            Thread.yield();
+        return notDone;
+    }
+}
+
+class anotherDaemon implements Runnable
+{
+    private Future[] exec;
+    private int notDone = -1;
+
+    anotherDaemon(Future[] exe)
+    {
+        exec = exe;
+    }
+
+    @Override
+    synchronized public void run()
+    {
+        while (!Thread.currentThread().isInterrupted())
+        {
+            int b = 0;
+            for (Future buf : exec)
+                if (!buf.isDone())
+                    b++;
+            notDone = b;
+        }
+    }
+
+    public int getNotDone()
+    {
+        while (notDone == -1)
+            Thread.yield();
         return notDone;
     }
 }
