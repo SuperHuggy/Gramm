40a5a029b0f81fc7acb0fa654b5aff9988507087
SuperHuggy <stolyarov_danil@mail.ru>
2019-04-19 10:20:10 +0300

    Модуль 5

--- Temp/src/Module5.java
+++ Temp/src/Module5.java
@@ -12,11 +12,17 @@ public class Module5
 
     public static int[][] KMP(String h, String[] n)
     {
+        Timer timer = new Timer();
         int[][] result = new int[n.length][];
-        ExecutorService exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() > n.length ? n.length : Runtime.getRuntime().availableProcessors());
+        ExecutorService exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         Future<ArrayList<Integer>>[] buf = new Future[n.length];
         for (int i = 0; i < n.length; i++)
             buf[i] = exec.submit(new MultyKMP(h, n[i]));
+        Daemon daemon = new Daemon(buf);
+        timer.schedule(daemon, 0, 1000);
+        while (daemon.getNotDone() != 0)
+            System.out.println(daemon.getNotDone());
+        timer.cancel();
         int i = 0;
         exec.shutdown();
         for (Future<ArrayList<Integer>> arr : buf)
@@ -39,6 +45,32 @@ public class Module5
 
 }
 
+class Daemon extends TimerTask
+{
+    private Future[] exec;
+    private int notDone;
+
+    Daemon(Future[] exe)
+    {
+        exec = exe;
+    }
+
+    @Override
+    public void run()
+    {
+        notDone = 0;
+        for (Future buf : exec)
+            if (!buf.isDone())
+                notDone++;
+        System.out.println(notDone);
+    }
+
+    public int getNotDone()
+    {
+        return notDone;
+    }
+}
+
 class MultyKMP implements Callable<ArrayList<Integer>>
 {
     MultyKMP(String haystack, String needle)
@@ -54,7 +86,6 @@ class MultyKMP implements Callable<ArrayList<Integer>>
     {
         return KMP(h, n);
     }
-
     private static ArrayList<Integer> KMP(String h, String n)
     {
         ArrayList<Integer> index = new ArrayList<>();
@@ -71,10 +102,7 @@ class MultyKMP implements Callable<ArrayList<Integer>>
                     j = 0;
                 }
             } else
-            {
-                i += j;
                 j = prefs[j];
-            }
         }
         return index;
     }
